a56deb6c6c4a0f9365955a76505eee8ccb17bb19
Amy Gori <amydgori@gmail.com>
2019-10-12 17:45:34 -0400

    Add Page class for organization

--- main.js
+++ main.js
@@ -1,12 +1,46 @@
-const el= document.querySelector('h1');
+class Page {
+  constructor() {
+    this.header = document.querySelector('h1');
+    this.nav = document.querySelector('.nav');
+    this.navLinks = document.querySelectorAll('.nav-link');
+    this.contentMountPoint = document.querySelector('.content-box');
 
-el.addEventListener('click', function(){
-  let p = document.querySelector('.description p')
-  if(p) {
-    p.parentNode.removeChild(p);
-  } else {
-    let p = document.createElement('p')
-    p.innerText = 'Answers for your questions'
-    document.querySelector('.description').appendChild(p)
+    this.contentText = {
+      aboutMe: 'Hello, this is about Adam',
+      contact: 'reading@songtarot.com',
+      aboutCards: 'Well this is text about the cards'
+    }
+
+    this.getText = (link) => {
+      switch (link) {
+        case 'nav-about-me':
+          return this.contentText.aboutMe;
+        case 'nav-contact':
+          return this.contentText.contact;
+        case 'nav-about-cards':
+          return this.contentText.aboutCards;
+        default:
+          return '';
+      }
+    }
   }
-});
+}
+
+window.onload = () => {
+  const page = new Page();
+  console.log(page.getText())
+  page.navLinks.forEach( (link) => {
+    link.addEventListener('click', (e) => {
+      let clickedLinkId = e.target.id;
+      let contentDiv = document.querySelector('.content-box>p');
+      console.log(clickedLinkId);
+      if (contentDiv) {
+        page.contentMountPoint.removeChild(contentDiv);
+      } else {
+        let p = document.createElement('p');
+        p.innerText = page.getText(clickedLinkId);
+        page.contentMountPoint.appendChild(p);
+      }
+    });
+  });
+}
