44752f3238fd4f0cbbdd9b02216cfa9a35c5fbf0
Amy Gori <amydgori@gmail.com>
2019-10-16 22:29:56 -0400

    Handle content swap and organize code a little better

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ class Page {
     this.nav = document.querySelector('.nav');
     this.navLinks = document.querySelectorAll('.nav-link');
     this.contentMountPoint = document.querySelector('.content-box');
-
     this.contentText = {
       about: 'I keep readings simple. You ask a question. I draw the cards. I look at the visual elements in the cards to find answers. Everything I say is verifiable in the cards. We can point to it. You don’t have to take my word for anything. I send you a photo of the cards and the reading in writing. You ask a clarifying question. I respond. $30.',
       aboutCards: 'I use Marseilles Tarot, Sibilla, Lenormandon, and playing cards. The cards are made of card stock. They are flat, nothing but air behind them. There’s nothing magic in them. The question that you ask is the magical element. We can think of a question as a line looking for its own source, and an answer as the line closing itself, a circle. Call and response. It’s that simple. Don’t ask me how it works. Who knows?'
+    }
+
+    this.setup = () => {
+      this.createNavMenu();
+      this.setViewportHeight();
     }
 
     this.getText = (link) => {
@@ -31,25 +35,36 @@ class Page {
         document.documentElement.style.setProperty('--vh', `${vh}px`);
       });
     }
+
+    this.handleContentChange = (e) => {
+      let clickedLinkId = e.target.id;
+      let contentDiv =  document.querySelector('.content-box>p');
+
+      if (contentDiv && contentDiv.className === clickedLinkId) {
+        this.contentMountPoint.removeChild(contentDiv);
+      } else {
+        let p = document.createElement('p');
+        p.innerText = this.getText(clickedLinkId);
+        p.className = clickedLinkId;
+
+        contentDiv ? this.contentMountPoint.replaceChild(p, contentDiv) : this.contentMountPoint.appendChild(p);
+      }
+    }
+
+    this.createNavMenu = () => {
+      this.navLinks.forEach((link) => {
+        link.addEventListener('click', (e) => {
+          console.log(e)
+          console.log(e.currentTarget)
+          console.log(e.target)
+          this.handleContentChange(e);
+        });
+      });
+    }
   }
 }
 
 window.onload = () => {
   const page = new Page();
-  page.navLinks.forEach( (link) => {
-    link.addEventListener('click', (e) => {
-      let clickedLinkId = e.target.id;
-      let contentDiv = document.querySelector('.content-box>p');
-      console.log(clickedLinkId);
-      if (contentDiv) {
-        page.contentMountPoint.removeChild(contentDiv);
-      } else {
-        let p = document.createElement('p');
-        p.innerText = page.getText(clickedLinkId);
-        page.contentMountPoint.appendChild(p);
-      }
-    });
-  });
-
-  page.setViewportHeight();
+  page.setup();
 }
