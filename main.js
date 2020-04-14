8c4f77c6dfa6a988edb464c936996eb6ba61f49b
Amy Gori <amydgori@gmail.com[core]>
2020-04-13 23:30:34 -0400

    Add writing section

--- main.js
+++ main.js
@@ -1,53 +1,58 @@
+import content from './writing-content.js'
+
 class Page {
-  constructor() {
-    this.header = document.querySelector('h1');
-    this.nav = document.querySelector('.nav');
-    this.navLinks = document.querySelectorAll('.nav-link');
-    this.contentMountPoint = document.querySelector('.content-box');
+  constructor () {
+    this.header = document.querySelector('h1')
+    this.nav = document.querySelector('.nav')
+    this.navLinks = document.querySelectorAll('.nav-link')
+    this.contentMountPoint = document.querySelector('.content-box')
     this.contentText = {
       about: 'I keep readings simple. You ask a question. I draw the cards. I look at the visual elements in the cards to find answers. Everything I say is verifiable in the cards. We can point to it. You don’t have to take my word for anything. I send you a photo of the cards and the reading in writing. You ask a clarifying question. I respond. $30.',
-      aboutCards: 'I use Marseilles Tarot, Sibilla, Lenormandon, and playing cards. The cards are made of card stock. They are flat, nothing but air behind them. There’s nothing magic in them. The question that you ask is the magical element. We can think of a question as a line looking for its own source, and an answer as the line closing itself, a circle. Call and response. It’s that simple. Don’t ask me how it works. Who knows?'
+      aboutCards: 'I use Marseilles Tarot, Sibilla, Lenormandon, and playing cards. The cards are made of card stock. They are flat, nothing but air behind them. There’s nothing magic in them. The question that you ask is the magical element. We can think of a question as a line looking for its own source, and an answer as the line closing itself, a circle. Call and response. It’s that simple. Don’t ask me how it works. Who knows?',
+      writing: content
     }
 
     this.setup = () => {
-      this.createNavMenu();
-      this.setViewportHeight();
+      this.createNavMenu()
+      this.setViewportHeight()
     }
 
     this.getText = (link) => {
       switch (link) {
         case 'nav-about':
-          return this.contentText.about;
+          return this.contentText.about
         case 'nav-about-cards':
-          return this.contentText.aboutCards;
+          return this.contentText.aboutCards
+        case 'nav-writing':
+          return this.contentText.writing
         default:
-          return '';
+          return ''
       }
     }
 
     this.setViewportHeight = () => {
       // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-      let vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
 
       window.addEventListener('resize', () => {
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-      });
+        const vh = window.innerHeight * 0.01
+        document.documentElement.style.setProperty('--vh', `${vh}px`)
+      })
     }
 
     this.handleContentChange = (e) => {
-      let clickedLinkId = e.target.id;
-      let contentDiv =  document.querySelector('.content-box>p');
+      const clickedLinkId = e.target.id
+      const contentDiv = document.querySelector('.content-box>div')
 
       if (contentDiv && contentDiv.className === clickedLinkId) {
-        this.contentMountPoint.removeChild(contentDiv);
+        this.contentMountPoint.removeChild(contentDiv)
       } else {
-        let p = document.createElement('p');
-        p.innerText = this.getText(clickedLinkId);
-        p.className = clickedLinkId;
+        const div = document.createElement('div')
+        div.innerHTML = this.getText(clickedLinkId)
+        div.className = clickedLinkId
 
-        contentDiv ? this.contentMountPoint.replaceChild(p, contentDiv) : this.contentMountPoint.appendChild(p);
+        contentDiv ? this.contentMountPoint.replaceChild(div, contentDiv) : this.contentMountPoint.appendChild(div)
       }
     }
 
@@ -57,14 +62,14 @@ class Page {
           console.log(e)
           console.log(e.currentTarget)
           console.log(e.target)
-          this.handleContentChange(e);
-        });
-      });
+          this.handleContentChange(e)
+        })
+      })
     }
   }
 }
 
 window.onload = () => {
-  const page = new Page();
-  page.setup();
+  const page = new Page()
+  page.setup()
 }
---
+++ writing-content.js
@@ -0,0 +1,80 @@
+const content = `
+<h2>Corona Virus</h2>
+<p>April 2020</p>
+
+<p>The other night I took my late night walk with the dog and stopped at a tree I often stop at. It's a mature Princeton
+  Elm tree, perhaps a survivor of the epidemic that killed most elms. It seems a fitting entity to turn to.</p>
+<p>I asked what it, or the trees, had to tell us right now in regard to all that is happening.</p>
+<p>Here's what we got.</p>
+<div class='card-layout'>
+  <img src="images/pandemic-cards.jpg" class='card-image'>
+  <div class='card-descriptions'>
+    <div>
+      <p>Three of Spades (Traps)</p>
+      <p>Q of Spades (Widow)</p>
+      <p>Ace of Hearts (A Sweet Card)</p>
+    </div>
+    <div>
+      <p>Q of Diamonds (Angry Woman)</p>
+      <p>Q of Hearts (Tenderness)</p>
+      <p>Nine of Spades (Death)</p>
+    </div>
+  </div>
+</div>
+<p>What first stuck me was the fact that we see nearly every queen in the deck. And I think we can say that the Ace of
+  Hearts is a stand in for a queen. So we have four queens. Six cards. Four Queens. This is a picture of a ruling female
+  presence. Perhaps the corona virus is just this. A queen bearing a crown.</p>
+
+<p>In each sequence we see the two faces of that feminine force. (I'll call her Mother Nature.) In the first sequence,
+  we see her as the cold queen (the Queen of Spades) and the warm queen (the Ace of Hearts). Her similar seated gesture
+  in each card is a clue that this is one entity, not two. So we have Mother Nature in her two aspects, seated, not
+  active, regarding the Three of Spades.</p>
+
+
+<p>In the next sequence, we see her on her feet. She is active now. Again we see her in her two aspects -- as destroying
+  force and as loving force. Following that we have the Nine of Spades. The whole set of cards begins and ends with
+  spades -- Three of Spades and Nine of Spades.</p>
+
+
+<p>Let's not forget that we are probably seeing things here from the perspective of a tree, or trees, so this is not a
+  human centric view. It's the human-centric worldview that probably has landed us in this place. So I'm hoping for an
+  aerial view here, a longer view, than the one we usually take.</p>
+
+<p>At any rate, we see in the first sequence of cards a passive Mother Nature regarding, or keeping an eye on, what is
+  happening in the Three of Spades (traps). Then we see an active Mother Nature. And then we get the Death card.</p>
+
+<p>I think it's safe to say that the only card in this whole set that represents human activity is the first card, the
+  Three of Spades. And I think it's safe to say that it represents a rapacious or deceptive relationship with nature.
+  The traps are a neat symbol of humans tricking and exploiting nature, and one another -- of humans exercising their
+  cunning to gain through deception. Or something like that. And we see that that behavior is being observed. It is not
+  going unnoticed.</p>
+
+<p>I imagine that the final sequence might make some people rather uncomfortable. But I think some of us can handle it's
+  impersonal statement. We see mother nature reacting to what has gone on above. She is in destroying mode. And yet she
+  also lovingly gathers her children. For the reaping. And yes, we can see this as a loving gesture, hard as it is to
+  swallow.</p>
+
+<p>The excruciating part of this is that the innocent, represented as children in the Queen of Hearts card, will pay for
+  this as well as those who we might see as the perpetrators of this.</p>
+
+<p>It is unfair from our point of view. But we are all part of nature. We are in this together. There are no exceptions.
+  And this is nature exercising its right to achieve balance. That's how I envision it, at least.</p>
+
+<p>And it is shown in the cards themselves. Two Black cards and one Red above. Two red cards and one black below.
+  Imbalance in each sequence, but balanced by the whole (three reds, three blacks). The universe does not allow
+  imbalance. In the end, everything is put back into balance.</p>
+
+<p>The lovely bit of this is knowing that in the end, all lies and all injustices are swept away. Nature won't allow us
+  to live this way.</p>
+
+<p>Lastly, and venturing further into the unknown, I see a possible connection between the first and last cards, the
+  Three of Spades and Nine of Spades.</p>
+
+<p>That fact that they are both spades and represent a multiple of three stands out to me. I'm not sure exactly what it
+  might mean, but since the final card is the reaper, and the first card represents the crime against natural law,
+  perhaps it's a suggestion as to the consequences.</p>
+<p>Three over nine. One third (or perhaps the remainder, two thirds). One third of humanity afflicted? I won't speculate
+  further than that, because I don't know. But I suspect there is something to it.</p>
+`
+
+export default content
