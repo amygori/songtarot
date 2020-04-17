2a9f728d487614f3ca30516a0078d67e1f71ab95
amygori <amydgori@gmail.com>
2020-04-17 13:55:07 -0400

    Update blog content

--- writing-content.js
+++ writing-content.js
@@ -1,68 +1,76 @@
 const content = `
-<h2>Corona Virus</h2>
+<h2>Coronavirus</h2>
 <p>April 2020</p>
 
-<p>The other night I took my late night walk with the dog and stopped at a tree I often stop at. It's a mature Princeton
-  Elm tree, perhaps a survivor of the epidemic that killed most elms. It seems a fitting entity to turn to.</p>
-<p>I asked what it, or the trees, had to tell us right now in regard to all that is happening.</p>
-<p>Here's what we got.</p>
-<div class='card-image'>
-  <img src="images/pandemic-cards.jpg">
-</div>
-<p>What first stuck me was the fact that we see nearly every queen in the deck. And I think we can say that the Ace of
-  Hearts is a stand in for a queen. So we have four queens. Six cards. Four Queens. This is a picture of a ruling female
-  presence. Perhaps the corona virus is just this. A queen bearing a crown.</p>
+<p>The other night I took a late walk with the dog and stopped at a tree I often stop at. It's a mature Princeton Elm tree, a survivor of the pandemic that killed most elms. It seems a fitting entity to turn to now.</p>
+<p>I asked what it had to tell us in regard to all that is happening.</p>
 
-<p>In each sequence we see the two faces of that feminine force. (I'll call her Mother Nature.) In the first sequence,
-  we see her as the cold queen (the Queen of Spades) and the warm queen (the Ace of Hearts). Her similar seated gesture
-  in each card is a clue that this is one entity, not two. So we have Mother Nature in her two aspects, seated, not
-  active, regarding the Three of Spades.</p>
+<div class = 'card-image' >
+  <img src = "images/pandemic-cards.jpg" >
+  </div>
+<p>The first card we get is the Three of Spades (Traps). In it a figure waits to snare a bird. This card is followed by the Queen of Spades (Widow). And then we get the Ace of Hearts (Sweet Card).</p>
 
+<p>The female figure depicted in the Queen of Spades is seated. Her posture and gaze are inclined toward the figure with the traps. Likewise, the female figure shown in the Ace of Hearts is seated. She also looks toward the figure with the traps. Both of these figures demonstrate a similar gesture. They point to the same thing.</p>
 
-<p>In the next sequence, we see her on her feet. She is active now. Again we see her in her two aspects -- as destroying
-  force and as loving force. Following that we have the Nine of Spades. The whole set of cards begins and ends with
-  spades -- Three of Spades and Nine of Spades.</p>
+<p>In the second sequence, the Queen of Diamonds (Angry Woman) is standing. She has tossed a table and chair to the ground in anger. Then the figure in the Queen of Hearts stoops to embrace children. Last, the figure of Death in the Nine of Spades stands facing us with its scythe and hourglass.</p>
 
+<p>What strikes me is that in these six cards we have nearly every queen in the deck. Only one is lacking. However, we do have a fourth female figure in these cards, in the Ace of Hearts. The similarity of her gesture to the Queen of Spades makes her analogous to that figure. Her gesture is a near perfect visual rhyme with that of the Queen of Spades. They are united in attitude. If we can see this unity, we can see a fourth queen, or four queenly entities, in a set of six cards.</p>
 
-<p>Let's not forget that we are probably seeing things here from the perspective of a tree, or trees, so this is not a
-  human centric view. It's the human-centric worldview that probably has landed us in this place. So I'm hoping for an
-  aerial view here, a longer view, than the one we usually take.</p>
+<p>And that gives us a definitive picture of a ruling female presence. But what does this ruling female presence have to say to us?</p>
 
-<p>At any rate, we see in the first sequence of cards a passive Mother Nature regarding, or keeping an eye on, what is
-  happening in the Three of Spades (traps). Then we see an active Mother Nature. And then we get the Death card.</p>
+<p>Let’s look more closely at the queens. In the first sequence, the analogous gestures of the two female figures (in the Queen of Spades and the Ace of Hearts) is a clue that these two figures represent a single body, rather than two separate ones. They are united in form.</p>
 
-<p>I think it's safe to say that the only card in this whole set that represents human activity is the first card, the
-  Three of Spades. And I think it's safe to say that it represents a rapacious or deceptive relationship with nature.
-  The traps are a neat symbol of humans tricking and exploiting nature, and one another -- of humans exercising their
-  cunning to gain through deception. Or something like that. And we see that that behavior is being observed. It is not
-  going unnoticed.</p>
+<p>We can see them as representing two facets of a single entity. The Queen of Spades is cool (black cards are cool). The Ace of Hearts is warm (red cards are warm). One suggests death (the widow), the other sweetness (a sweet card). They are united in form. Moreover, they unite the duality that they represent.</p>
 
-<p>I imagine that the final sequence might make some people rather uncomfortable. But I think some of us can handle it's
-  impersonal statement. We see mother nature reacting to what has gone on above. She is in destroying mode. And yet she
-  also lovingly gathers her children. For the reaping. And yes, we can see this as a loving gesture, hard as it is to
-  swallow.</p>
+<p>In the second sequence, we see the two faces of the queen once again. She is standing now. She has become active. We see her in her two active aspects, as destroying force (the Angry Woman) and as creative or nurturing force (Tenderness).</p>
 
-<p>The excruciating part of this is that the innocent, represented as children in the Queen of Hearts card, will pay for
-  this as well as those who we might see as the perpetrators of this.</p>
+<p>Following these two faces of the queen entity, these combined opposing forces, we get the final card, the Nine of Spades, or La Mort (Death).</p>
 
-<p>It is unfair from our point of view. But we are all part of nature. We are in this together. There are no exceptions.
-  And this is nature exercising its right to achieve balance. That's how I envision it, at least.</p>
+<p>And in that French word, we get a reiteration and an accentuation of the dual nature of this feminine force. “La Mort” means death. But “La Mort” is pronounced exactly like the French word “L’Amour,” which means love.</p>
 
-<p>And it is shown in the cards themselves. Two Black cards and one Red above. Two red cards and one black below.
-  Imbalance in each sequence, but balanced by the whole (three reds, three blacks). The universe does not allow
-  imbalance. In the end, everything is put back into balance.</p>
+<p>Both those meanings exist together in the intonation of the word. Here we get the repetition of this dual nature operating simultaneously -- cold and warm, destroying and embracing, death and love.</p>
 
-<p>The lovely bit of this is knowing that in the end, all lies and all injustices are swept away. Nature won't allow us
-  to live this way.</p>
+<p>These images evoke the creative and destructive forces of Mother Nature. I am inclined to think of this ruling female presence that demonstrates a dual nature as Mother Nature herself.</p>
 
-<p>Lastly, and venturing further into the unknown, I see a possible connection between the first and last cards, the
-  Three of Spades and Nine of Spades.</p>
+<p>The prevalence of queens (two-thirds of the cards are queens) is itself a significant statement. The cards are filled with Mother Nature. Her presence calls out to us. If there is a sign crying out in these cards, it is the centrality of this ruling female presence, as well as the balance inherent in that presence, cool and warm, creative and destructive. That encapsulation of the binary, that active-passive balancing act, is the function we see these queens enacting.</p>
 
-<p>That fact that they are both spades and represent a multiple of three stands out to me. I'm not sure exactly what it
-  might mean, but since the final card is the reaper, and the first card represents the crime against natural law,
-  perhaps it's a suggestion as to the consequences.</p>
-<p>Three over nine. One third (or perhaps the remainder, two thirds). One third of humanity afflicted? I won't speculate
-  further than that, because I don't know. But I suspect there is something to it.</p>
+<p>Humans get short shrift here. I think the only card here that represents human activity is the very first card, the Three of Spades. It represents a predatory or deceptive relationship with nature.</p>
+
+<p>The traps shown in that card are a neat symbol of humans tricking and exploiting nature -- of humans exercising their cunning to gain through deception. Or something like that. And we see in the following two cards that that behavior is being observed. It is not going unnoticed.</p>
+
+<p>So we begin to sense that these cards present an assertion of nature’s balance. Perhaps the scythe we see in the hand of Death is meant to cut away what has caused the universal scales to tip out of balance.</p>
+
+<p>It is conspicuous that the whole set of six cards begins and ends with the suit of spades. The Three of Spades (Traps) begins it, and the Nine of Spades (Death) ends it.</p>
+
+<p>In traditional cartomancy, the suit of spades represents what we might describe as life’s challenges: sickness, death, hardship. That is to say, the suit of spades conventionally represents the hard lessons of human life on earth. We don’t have to adhere to those meanings, of course. We can see much more than that in the figure of a spade. But we can certainly hold those associations in the back of our minds as we proceed.</p>
+
+<p>Let's not forget that we are trying to see things from the perspective of a tree, if you are willing to accept that possibility. We are not seeking a human-centric point of view, but what we might describe as an “aerial” view, a longer or broader view than the one we habitually take.</p>
+
+<p>In the final sequence, we see Mother Nature reacting to what has gone on above, as represented in the Three of Clubs. She is in destroying mode, represented by the Queen of Diamonds. And yet she also, as represented in the Queen of Hearts, lovingly gathers her children. For the reaping (the Nine of Spades).</p>
+
+<p>This final sequence is a tough pill to swallow. A destructive Mother Nature lovingly gathers her children. The Death card follows, with its sharp blade pointed to the children. How do we reconcile these two opposing forces seemingly at work simultaneously? It’s an uncomfortable dichotomy to sit with.</p>
+
+<p>Perhaps if we can view this impersonally, then, even if we are unable to understand it, we can at least accept it as a reality. Because what is in the highest good of Nature (what is in the highest good of all life everywhere for that matter) may not take into account human comfort, or what humans feel is in their best interest.</p>
+
+<p>The excruciating part of this, for us as sympathetic humans, is that the innocent, represented as children in the Queen of Hearts card, will be called to account for this equally alongside those whom we might see as culpable.</p>
+
+<p>It seems indiscriminate and unfair. Perhaps it is. But we are all part of nature. None of us stands apart from the inescapable balancing act that governs the universal laws of Nature. We are all inextricably bound to it. There are no exceptions. And what we are seeing represented in these cards, it seems to me, is Nature exercising its right to restore balance.</p>
+
+<p>This balancing act is shown in the cards themselves. In the first sequence, we see two black cards and one red. In the second sequence, we see two red cards and one black. Imbalance in each sequence, but balanced by the whole (three reds, three blacks). The universe corrects imbalance. In the end, everything is put into true.</p>
+
+<p>The lovely bit of this is knowing that in the end, all lies and all injustices, what is represented in the Three of Spades, are swept away. Nature won't allow us to live this way, even if we can’t help ourselves, or even if we feel helpless to set things right.</p>
+
+<p>Lastly, and venturing further into the unknown, I see a possible connection between the first and last cards, the Three of Spades and Nine of Spades.</p>
+
+<p>That fact that they are both in the suit of spades and represent a multiple of three stands out to me. I'm not sure what it might mean, but since the first card suggests the crime against nature, and the final card shows the reaper, perhaps it's a suggestion as to the consequences.</p>
+
+<p>Three over nine. One-third (or perhaps the remainder, two-thirds). One third of humanity afflicted? I won't speculate further than that, because I don't know. (And sometimes we aren’t meant to know.)</p>
+
+<p>It’s also interesting when we remember that the queens represent two-thirds of the cards in this reading, the spades one-third, reinforcing the mathematical concept of thirds.</p>
+
+<p>Two-thirds of the cards belong to Mother Nature. One-third belongs to human activity and its consequences -- and those cards are spades, which if we revisit the traditional meaning of spades, represent life’s hard lessons. It certainly is suggestive.</p>
+
+<p>Your guess is as good as mine as to what that might mean. But it does seem, in the glimpse we have gotten in these cards, that Mother Nature has all hands on deck. And perhaps we’re on her timeline now. And hers is a timeline that is not scaled to human lives, but to the balance of all life.</p>
 `
 
 export default content
