99019d357bd7521c2b20a7aaf17f2e5e34d10b20
Amy Gori <amydgori@gmail.com>
2020-04-17 23:03:21 -0400

    Move methods out of the constructor

--- js/main.js
+++ js/main.js
@@ -12,59 +12,60 @@ class Page {
       aboutCards: content.cards,
       writing: content.writing
     }
+  }
 
-    this.setup = () => {
-      this.createNavMenu()
-      this.setViewportHeight()
-    }
+  setup () {
+    this.createNavMenu()
+    this.setViewportHeight()
+  }
 
-    this.getText = (link) => {
-      switch (link) {
-        case 'nav-about':
-          return this.contentText.about
-        case 'nav-about-cards':
-          return this.contentText.aboutCards
-        case 'nav-writing':
-          return this.contentText.writing
-        default:
-          return ''
-      }
-    }
-
-    this.setViewportHeight = () => {
-      // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-      const vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty('--vh', `${vh}px`)
-
-      window.addEventListener('resize', () => {
-        const vh = window.innerHeight * 0.01
-        document.documentElement.style.setProperty('--vh', `${vh}px`)
-      })
-    }
-
-    this.handleContentChange = (e) => {
-      const clickedLinkId = e.target.id
-      const contentDiv = document.querySelector('.content-box>div')
-
-      if (contentDiv && contentDiv.className === clickedLinkId) {
-        this.contentMountPoint.removeChild(contentDiv)
-      } else {
-        const div = document.createElement('div')
-        div.innerHTML = this.getText(clickedLinkId)
-        div.className = clickedLinkId
-
-        contentDiv ? this.contentMountPoint.replaceChild(div, contentDiv) : this.contentMountPoint.appendChild(div)
-      }
-    }
-
-    this.createNavMenu = () => {
-      this.navLinks.forEach((link) => {
-        link.addEventListener('click', (e) => {
-          this.handleContentChange(e)
-        })
-      })
+  getText (link) {
+    switch (link) {
+      case 'nav-about':
+        return this.contentText.about
+      case 'nav-about-cards':
+        return this.contentText.aboutCards
+      case 'nav-writing':
+        return this.contentText.writing
+      default:
+        return ''
     }
   }
+
+  setViewportHeight () {
+    // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+    const vh = window.innerHeight * 0.01
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+    window.addEventListener('resize', () => {
+      const vh = window.innerHeight * 0.01
+      document.documentElement.style.setProperty('--vh', `${vh}px`)
+    })
+  }
+
+  handleContentChange (e) {
+    const clickedLinkId = e.target.id
+    const contentDiv = document.querySelector('.content-box>div')
+
+    if (contentDiv && contentDiv.className === clickedLinkId) {
+      this.contentMountPoint.removeChild(contentDiv)
+    } else {
+      const div = document.createElement('div')
+      div.innerHTML = this.getText(clickedLinkId)
+      div.className = clickedLinkId
+
+      contentDiv ? this.contentMountPoint.replaceChild(div, contentDiv) : this.contentMountPoint.appendChild(div)
+    }
+  }
+
+  createNavMenu () {
+    this.navLinks.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        this.handleContentChange(e)
+      })
+    })
+  }
+
 }
 
 window.onload = () => {
